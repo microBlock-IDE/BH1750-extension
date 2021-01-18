3cbf592db7148dac3a285b5c7ab1ada570f34863
Max <sonthaya.nt@hotmail.com>
2021-01-18 21:36:43 +0700

    Add support IPST-WiFi

--- modules/BH1750.py
+++ modules/BH1750.py
@@ -1,12 +1,17 @@
 from machine import Pin
 from machine import I2C
 from time import sleep
+import os
 
 BH1750_ADDR = None
 
 def read(addr=0x23):
     global BH1750_ADDR
-    i2c1 = I2C(1, scl=Pin(5), sda=Pin(4), freq=100000)
+    machine = os.uname().machine
+    if "KidBright32" in machine:
+        i2c1 = I2C(1, scl=Pin(5), sda=Pin(4), freq=100000)
+    else:
+        i2c1 = I2C(0, scl=Pin(22), sda=Pin(21), freq=100000)
     if addr != BH1750_ADDR:
         BH1750_ADDR = addr
         i2c1.writeto(BH1750_ADDR, b'\x10')
