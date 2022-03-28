dc17afb882cfd327911a8791f1460fdf68ff6f21
Max <sonthaya.nt@hotmail.com>
2022-03-28 14:05:03 +0700

    Update BH1750.py

--- modules/BH1750.py
+++ modules/BH1750.py
@@ -8,8 +8,10 @@ BH1750_ADDR = None
 def read(addr=0x23):
     global BH1750_ADDR
     machine = os.uname().machine
-    if "KidBright32" in machine:
+    if ("KidBright32" in machine) or ("KidMotor V4" in machine):
         i2c1 = I2C(1, scl=Pin(5), sda=Pin(4), freq=100000)
+    elif "Mbits" in machine:
+        i2c1 = I2C(0, scl=Pin(21), sda=Pin(22), freq=100000)
     else:
         i2c1 = I2C(0, scl=Pin(22), sda=Pin(21), freq=100000)
     if addr != BH1750_ADDR:
