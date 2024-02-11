fa7f604440758164dc43ec1fddd483534b6d0a85
Max <7087907+maxpromer@users.noreply.github.com>
2024-02-11 23:14:00 +0700

    Add Arduino support

--- blocks/generators.js
+++ blocks/generators.js
@@ -6,3 +6,33 @@ Blockly.Python['bh1750_light'] = function (block) {
   var code = `BH1750.read(${dropdown_addr})`;
   return [code, Blockly.Python.ORDER_NONE];
 };
+
+Blockly.JavaScript['bh1750_light'] = function (block) {
+  Blockly.JavaScript.definitions_['include']['ArtronShop_BH1750.h'] = '#include <ArtronShop_BH1750.h>';
+
+  var dropdown_addr = block.getFieldValue('addr');
+
+  var functionName = Blockly.JavaScript.provideFunction_(
+    `bh1750_read`,
+    [
+      'float ' + Blockly.JavaScript.FUNCTION_NAME_PLACEHOLDER_ + '(int addr) {',
+      '  static ArtronShop_BH1750 * bh1750 = NULL;',
+      '  static uint8_t last_addr = 0;',
+      '  if ((!bh1750) || (last_addr != addr)) {',
+      '    if (bh1750) {',
+      '      delete bh1750;',
+      '      bh1750 = NULL;',
+      '    }',
+      '    bh1750 = new ArtronShop_BH1750(addr, &Wire);',
+      '    Wire.begin();',
+      '    bh1750->begin();',
+      '    last_addr = addr;',
+      '  }',
+      `  return bh1750->light();`,
+      '}'
+    ]
+  );
+
+  var code = `${functionName}(${dropdown_addr})`;
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
--- extension.js
+++ extension.js
@@ -3,10 +3,14 @@
     description: "Ambient Light Sensor",
     author: "microBlock",
     category: "Sensors",
-    version: "1.0.0",
+    version: "1.1.0",
     icon: "/static/icon.png", // Category icon
     color: "#006a99", // Category color (recommend some blocks color)
     blocks: [ // Blocks in Category
         "bh1750_light"
+    ], 
+    supportArduinoPlatform: true,
+    depends: [ // Arduino library
+        "ArtronShop_BH1750@1.0.0"
     ]
 });
